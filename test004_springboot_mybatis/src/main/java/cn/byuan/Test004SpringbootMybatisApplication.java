5055f78adb0b326c7c2feed7dd1316b83342f782
byuan98 <byuan98@outlook.com>
2021-07-05 15:57:15 +0800

    Spring integration mybatis

---
+++ test004_springboot_mybatis/src/main/java/cn/byuan/Test004SpringbootMybatisApplication.java
@@ -0,0 +1,13 @@
+package cn.byuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test004SpringbootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test004SpringbootMybatisApplication.class, args);
+    }
+
+}
